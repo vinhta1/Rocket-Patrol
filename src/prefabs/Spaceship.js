4da5e00195427f51a371193b46121dc8cd8711b9
Vinh Ta <tavinh132@gmail.com>
2023-10-14 13:38:18 -0700

    up until ship points

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -13,7 +13,12 @@ class Spaceship extends Phaser.GameObjects.Sprite {
 
         //wrap around from left edge to right edge
         if (this.x <= 0 - this.width) {
-            this.x = game.config.width;
+            this.reset();
         }
+    }
+
+    //position reset
+    reset() {
+        this.x = game.config.width;
     }
 }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,6 +8,7 @@ class Play extends Phaser.Scene {
         this.load.image("rocket", "./assets/images/rocket.png");
         this.load.image("spaceship", "./assets/images/spaceship.png");
         this.load.image("starfield", "./assets/images/starfield.png");
+        this.load.spritesheet("explosion", "./assets/images/explosion.png", {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
     }
 
     create() {
@@ -35,6 +36,31 @@ class Play extends Phaser.Scene {
         this.ship01 = new Spaceship(this, game.config.width + borderUISize * 6, borderUISize * 4, "spaceship", 0, 30).setOrigin(0, 0); //top ship
         this.ship02 = new Spaceship(this, game.config.width + borderUISize * 3, borderUISize * 5 + borderPadding * 2, "spaceship", 0, 20).setOrigin(0,0); //middle ship
         this.ship03 = new Spaceship(this, game.config.width, borderUISize * 6 + borderPadding * 4, "spaceship", 0, 10).setOrigin(0, 0); //bottom ship
+    
+        //animation config
+        this.anims.create({
+            key: "explode",
+            frames: this.anims.generateFrameNumbers("explosion", {start: 0, end: 9, first: 0}),
+            frameRate: 30
+        });
+
+        //initialize score
+        this.p1Score = 0;
+
+        //display score
+        let scoreConfig = {
+            fontFamily: "Courier",
+            fontSize: "28px",
+            backgroundColor: "#F3B141",
+            color: "#843605",
+            align: "right",
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 100
+        }
+        this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding * 2, this.p1Score, scoreConfig);
     }
 
     update() {
@@ -43,5 +69,47 @@ class Play extends Phaser.Scene {
         this.ship01.update();
         this.ship02.update();
         this.ship03.update();
+
+        //check collisions
+        if(this.checkCollision(this.p1Rocket, this.ship03)){
+            this.p1Rocket.reset();
+            this.shipExplode(this.ship03)
+        }
+        if(this.checkCollision(this.p1Rocket, this.ship02)){
+            this.p1Rocket.reset();
+            this.shipExplode(this.ship02)
+        }
+        if(this.checkCollision(this.p1Rocket, this.ship01)){
+            this.p1Rocket.reset();
+            this.shipExplode(this.ship01)
+        }
+    }
+
+    checkCollision(rocket, ship) {
+        //simple AABB checking
+        if (rocket.x < ship.x + ship.width && //leftmost rocket is touching left of rightmost ship
+            rocket.x + rocket.width > ship.x && //rightmost rocket is touching right of leftmost ship
+            rocket.y < ship.y + ship.height && // topmost rocket is touching bottom ship
+            rocket.height + rocket.y > ship.y) {//bottom rocket is under top ship
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    shipExplode(ship) {
+        //temporarily hide ship
+        ship.alpha = 0;
+        //create explosion sprite at ship's position
+        let boom = this.add.sprite(ship.x, ship.y, "explosion").setOrigin(0, 0);
+        boom.anims.play("explode"); //play explode animation
+        boom.on("animationcomplete", () => {    //callback after anim completes, () => makes temp function
+        ship.reset();                           //reset ship position
+        ship.alpha = 1;                         //make ship visible again
+        boom.destroy();                         //remove explosion sprite
+        });
+        //score add and repaint
+        this.p1Score += ship.points;
+        this.scoreLeft.text = this.p1Score;
     }
 }
